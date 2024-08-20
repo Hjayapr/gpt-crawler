48d12d4451041e0e17307bab587caf363dc6378e
Hjayapr <hunsica.jayaprakash@teledyne.com>
2024-08-20 14:34:11 -0400

    trial2
    trial2

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.federalcompass.com/federal-government-awarded-contracts",
   match: "https://www.federalcompass.com/award-contract-detail/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  maxPagesToCrawl: 100,
+  outputFileName: "output2.json",
   maxTokens: 2000000,
 };
